c7b32550491784ce8697400b1e38af598cc923d3
YusukeNakaya <yusuke.nakaya@jp.oro.com>
2018-04-03 20:20:24 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# date-countdown
+
+Just by specifying the year, month, day, the remaining number of days until that day is returned.
+
+## Installation
+
+### npm
+
+```
+$ npm install date-countdown
+```
+
+### unpkg
+
+```html
+<script src="https://unpkg.com/date-countdown" type="text/javascript"></script>
+```
+
+## Usage
+
+```javascript
+const daysLeft = dateCountdown(2020, 7, 24)
+console.log(daysLeft) // Days left until　"2020 Tokyo Olympics"
+```
+
+## License
+
+MIT
---
+++ index.browser.js
@@ -0,0 +1,13 @@
+window.dateCountdown = function dateCountdown(year, month, date) {
+  let result
+  const dateObj = new Date()
+  const todayYear = dateObj.getFullYear()
+  const todayMonth = dateObj.getMonth()
+  const todayDate = dateObj.getDate()
+  const eventUTC = Date.UTC(year, month - 1, date)
+  const todayUTC = Date.UTC(todayYear, todayMonth, todayDate)
+  const eventDay = (eventUTC - todayUTC) / 1000 / 60 / 60 / 24
+  if (eventDay < 0) result = 0
+  else result = eventDay
+  return result
+}
